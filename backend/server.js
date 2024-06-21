2effd328f6588431302437330681c8e477623681
Aryan <raj.aryan.31077@gmail.com>
2024-06-22 01:40:48 +0530

    chore: Updated MongoDB connection handling

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-import express from "express"
+import express from "express";
 //import path from "path";
 //import cookieParser from "cookie-parser"
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-import authroutes from "./routes/auth.routes.js"
+import authroutes from "./routes/auth.routes.js";
 
-import connectToMongoDB from "./db/connectToMongodb.js"
+import connectToMongoDB from "./db/connectToMongodb.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use("/api/auth",authroutes);
+app.use("/api/auth", authroutes);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
@@ -21,7 +21,6 @@ app.use("/api/auth",authroutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-   connectToMongoDB();
-   console.log(`Server started at port no. : ${PORT}`)
-
+  connectToMongoDB();
+  console.log(`Server started at port no. : ${PORT}`);
 });
