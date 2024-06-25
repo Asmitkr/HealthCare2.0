044b764571d390f3314d64950fec43680730c060
undeadlich <piyushnama3@gamil.com>
2024-06-25 16:18:28 +0530

    cookie auth fixed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 //import path from "path";
-//import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser"
 import dotenv from "dotenv";
 
 import authroutes from "./routes/auth.routes.js";
@@ -13,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authroutes);
 app.use("/api/insurance",insuroutes);
